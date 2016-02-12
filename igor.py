84da2d458e2b564c2e2bcb4bc1ed7f6b898ebb4e
dumheter <christoffer.golf@gmail.com>
2016-02-12 13:27:29 +0100

    init

---
+++ igor.py
@@ -0,0 +1,35 @@
+# TODO
+# * Don't use sleep
+# * Count cps
+
+import win32api, win32con, time
+
+# left click at given cordinates
+def click (x, y):
+  win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
+  win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
+
+# Igor provides what others can't.
+def igor ():
+    a, b = win32api.GetCursorPos()
+    ref = a
+    
+    while (ref == a):
+        a, b = win32api.GetCursorPos()
+        click(a, b)
+        time.sleep(0.001) #minumum os sleeptime is enugh
+  
+def main ():
+    print("==========================")
+    print("=== Move the mouse to exit")
+    ans = input("=== type >s< to start: ")
+
+    if (ans == "s"):
+        igor()
+
+    print("==========================")
+    exit(1)
+
+
+if __name__ == "__main__":
+    main()
