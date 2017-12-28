b37587581931e2fd62bad81b56a4773737690345
Christoffer Gustafsson <christoffer.golf@gmail.com>
2017-12-28 17:29:29 +0100

    Added some features.

--- igor.py
+++ igor.py
@@ -1,18 +1,20 @@
 # @delay minimum time in seconds between each click
 #        chrome browser has trouble handling anything
 #        below this number.
-DELAY = 0.2
+#DELAY = 0.2
+DELAY = 0.05
+DELAY_STEP = 0.01
 
 import win32api, win32con, time, os
 
 # left click at given cordinates
-# @_click=0 -> left click
-# @_click=1 -> right click
+# @_click=1 -> left click
+# @_click=2 -> right click
 def click (x, y, _click) -> int:
-  if _click == 0:
+  if _click == 1:
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
-  elif _click == 1:
+  elif _click == 2:
     win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTDOWN,x,y,0,0)
     win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTUP,x,y,0,0)
   return 1
@@ -38,9 +40,11 @@ def igor (_click):
 def keyboardHandler():
   _click = 0
   _esc = False
+  win32api.SetConsoleTitle("paused")
   while (not _esc):
     if (_click > 0):
       igor(_click)
+      win32api.SetConsoleTitle("paused")
       print("\033[91m" + "= Cursor movement, stoped clicking." + "\033[0m")
 
     _click = 0
@@ -54,6 +58,18 @@ def keyboardHandler():
         _click = 2
         print("\033[92m" + "= F10 pressed, now right clicking!" + "\033[0m")
 
+      elif (abs(win32api.GetKeyState(119)) > 1):
+        global DELAY
+        DELAY += DELAY_STEP
+        print("\033[92m" + "= F8 pressed, increasing delay to " + str(DELAY) + "\033[0m")
+        time.sleep(0.2)
+
+      elif (abs(win32api.GetKeyState(118)) > 1):
+        global delay
+        DELAY -= DELAY_STEP
+        print("\033[92m" + "= F8 pressed, decreasing delay to " + str(DELAY) + "\033[0m")
+        time.sleep(0.2)
+
       elif (abs(win32api.GetKeyState(0x1B)) > 1):
         _esc = True
         print("= ESC pressed, exiting")
@@ -64,11 +80,12 @@ def keyboardHandler():
 def main():
   print("========================")
   print("= Welcome " + os.getlogin())
-  print("= Press F9 to start/resume.")
   print("= Move the cursor to pause")
-  print("= Press ESC to exit.")
   print("= Clicks/s is displayed in the title.")
   print("=========================")
+  print("= Press ESC to exit.")
+  print("= Press F7 to decrease time between clicks")
+  print("= Press F8 to increase time between clicks")
   print("= Press F9  to begin auto left  clicking")
   print("= Press F10 to begin auto right clicking")
   keyboardHandler()
